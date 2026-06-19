0ba2b25705694e3f06396730c40c3f2e105edcee
linghao <1476195143@qq.com>
2026-06-19 18:25:22 +0800

    cleanup: remove 12 hand-written stubs replaced by proper generators
    Removed from camx_stubs/ (now generated by props.pl/settingsgenerator.pl/ParameterParser):
      g_camxsettings.h, g_camxproperties.h, g_camxtitan17xsettings.h,
      camxsensordriver.h, camxeepromdriver.h, camxeebindriver.h,
      camxflashdriver.h, camxactuatordriver.h, camxpdafconfig.h,
      imagesensormodulesetmanager.h, parametertuningtypes.h, camximagesensorutils.h
    
    Remaining 10 stubs in camx_stubs/ are NOT auto-generated:
      camxmem.h, camxexternalsensor.h, camxstatsdebugdatatypes.h,
      tuningsetmanager.h, ipdefs.h, camxifestripinginterface.h,
      qdMetaData.h, android/log.h, cutils/properties.h, utils/Log.h
    
    TestBayerToYUV PASS with EXIT=0.

--- camx_stubs/camxpdafconfig.h
+++
@@ -1,4 +0,0 @@
-#ifndef STUB_CAMXPDAFCONFIG_H
-#define STUB_CAMXPDAFCONFIG_H
-enum class PDAFType { PDTypeUnknown=0,PDType1=1,PDType2=2,PDType3=3,PDType2PD=4,MAX=5 };
-#endif
--- camx_stubs/g_camxtitan17xsettings.h
+++
@@ -1,7 +0,0 @@
-#ifndef G_CAMXTITAN17XSETTINGS_H
-#define G_CAMXTITAN17XSETTINGS_H
-#include "g_camxsettings.h"
-CAMX_NAMESPACE_BEGIN
-struct Titan17xStaticSettings : public StaticSettings {};
-CAMX_NAMESPACE_END
-#endif
